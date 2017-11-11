0e6e9ced8ce272690bbe2660ffb502d2c0ad482b
Ashira <timecrisis1995@live.com>
2017-11-12 01:52:46 +0700

    UpdateFirebase
    Add Script firebase

---
+++ Assets/firebase.cs
@@ -0,0 +1,25 @@
+﻿using Firebase;
+using Firebase.Database;
+using Firebase.Unity.Editor;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class firebase : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		FirebaseApp.DefaultInstance.SetEditorDatabaseUrl("hhttps://middlefear.firebaseio.com/");
+		DatabaseReference reference = FirebaseDatabase.DefaultInstance.RootReference;
+		reference.Child("position").SetValueAsync("hello");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		
+	}
+}
